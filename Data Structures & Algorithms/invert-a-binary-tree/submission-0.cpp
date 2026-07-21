1e813b39cd657a6430c9283b26526b107645af99
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-21 23:59:38 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* helper(TreeNode* root){
+        if(!root){return nullptr;}
+        TreeNode* left= helper(root->left);
+        TreeNode* right=helper(root->right);
+        
+        // if(root->left){
+        //     right=root->left;
+        // }
+        // if(root->right){
+        //     left=root->right;
+        // }
+        root->left=right; root->right=left;
+        return root;
+        
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        return helper(root);
+    }
+};
